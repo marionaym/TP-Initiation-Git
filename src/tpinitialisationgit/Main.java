b208ddcf8cd00b3857c12d6fc2a7a7f1d86de7b1
marionymd <marionn.aymm@gmail.com>
2021-11-15 17:23:25 +0100

    first commit

---
+++ src/tpinitialisationgit/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpinitialisationgit;
+
+/**
+ *
+ * @author mario
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    // Test 1 2 Test
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
